69c758fbb0ab4d6cf6c244ccf1a1801e9b5bab64
sch-intern <anumjabeen02@gmail.com>
2023-09-27 18:29:45 -0400

    updated

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Header from "./components/Homepage/Header";
+import Body from "./components/Homepage/Body";
 import Footer from "./components/Homepage/Footer";
 
 function App() {
   return (
     <div>
       <Header/>
+      <Body/>
       <Footer/>
     </div>
   );
