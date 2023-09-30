c0bc941659dabf9609fe8ab2165e5fe3b655833f
sch-intern <anumjabeen02@gmail.com>
2023-09-30 02:15:40 -0400

    fixed categories ui

--- src/components/Homepage/Footer.js
+++ src/components/Homepage/Footer.js
@@ -1,62 +1,125 @@
-// src/components/Homepage/Footer.js
+  // src/components/Homepage/Footer.js
 
-import React from "react"
-import styled from "styled-components"
-import Accessories from "../../images/accessories.png"
-import Electronics from "../../images/electronics.png"
-import Pets from "../../images/pets.png"
-import Beauty from "../../images/beauty.png"
-import Gaming from "../../images/gaming.png"
-import School from "../../images/school.png"
-import Camera from "../../images/camera.png"
-import Home from "../../images/home.png"
-import Sports from "../../images/sports.png"
+  import React from "react"
+  import styled from "styled-components"
+  import Grid from '@mui/material/Grid';
+  import BuildIcon from '@mui/icons-material/Build';
+  import ComputerIcon from '@mui/icons-material/Computer';
+  import PetsIcon from '@mui/icons-material/Pets';
+  import BrushIcon from '@mui/icons-material/Brush';
+  import SportsEsportsIcon from '@mui/icons-material/SportsEsports';
+  import SchoolIcon from '@mui/icons-material/School';
+  import PhotoCameraBackIcon from '@mui/icons-material/PhotoCameraBack';
+  import HouseIcon from '@mui/icons-material/House';
+  import SportsBasketballIcon from '@mui/icons-material/SportsBasketball';
 
-function Footer (){
-  return (
-    <Wrapper>
-        <ImageContainer>
-        <img src = {Accessories} alt = "Accessories"/> Accessories
-        <img src = {Electronics} alt = "Electronics"/> Electronics
-        <img src = {Pets} alt = "Pets"/> Pets
-        </ImageContainer>
+  const Footer = () => {
+    return (
+      <Wrapper>
+          <TextContainer>
+              <StyledH1>Categories</StyledH1>
+          </TextContainer>
+            <Grid container spacing={6} direction="row" justifyContent="space-around"   alignItems="flex-end" rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3}}> 
+              <IconContainer>
+                <Grid item xs={6}>
+                    <IconWrapper>
+                      <BuildIcon fontSize="medium" />
+                      <IconLabel>Accessories</IconLabel>
+                    </IconWrapper>
+                </Grid>
 
-        <ImageContainer>
-        <img src = {Beauty} alt = "Beauty"/> Beauty
-        <img src = {Gaming} alt = "Gaming"/> Gaming
-        <img src = {School} alt = "School"/> School
-        </ImageContainer>
+                <Grid item xs={6}>
+                    <IconWrapper>
+                      <ComputerIcon fontSize="medium" />
+                      <IconLabel>Electronics</IconLabel>
+                    </IconWrapper>
+                </Grid>
 
-        <ImageContainer>
-        <img src = {Camera} alt = "Camera"/> Camera
-        <img src = {Home} alt = "Home"/> Home
-        <img src = {Sports} alt = "Sports"/> Sports
-        </ImageContainer>
+                <Grid item xs={6}>
+                    <IconWrapper>
+                      <PetsIcon fontSize="medium" />
+                      <IconLabel>Pets</IconLabel>
+                    </IconWrapper>
+                </Grid>
 
-    </Wrapper>
-  )
-}
+                <Grid item xs={6}>
+                    <IconWrapper>
+                      <BrushIcon fontSize="medium" />
+                      <IconLabel>Beauty</IconLabel>
+                    </IconWrapper>
+                </Grid>
 
-export default Footer;
-const Wrapper = styled.div`
-    display: flex-wrap:wrap;
-    background-color: lightblue;
-    padding: 100px;
-    bottom: 0;
-    margin-top:200px;
-    margin-bottom:10px;
-    position: relative;
-`;
+                <Grid item xs={6}>
+                    <IconWrapper>
+                      <SportsEsportsIcon fontSize="medium" />
+                      <IconLabel>Gaming</IconLabel>
+                    </IconWrapper>
+                </Grid>
 
-const ImageContainer = styled.div`
-    img {
-        display: flex-wrap:nowrap
-        max-width: 20px; 
-        height: 20px;
-        position: relative;
-        padding: 10px;
-    }
-`; 
+                <Grid item xs={6}>
+                    <IconWrapper>
+                      <SchoolIcon fontSize="medium" />
+                      <IconLabel>School</IconLabel>
+                    </IconWrapper>
+                </Grid>
 
+                <Grid item xs={6}>
+                    <IconWrapper>
+                      <PhotoCameraBackIcon fontSize="medium" />
+                      <IconLabel>Camera</IconLabel>
+                    </IconWrapper>
+                </Grid>
 
+                <Grid item xs={6}>
+                    <IconWrapper>
+                      <HouseIcon fontSize="medium" />
+                      <IconLabel>Home</IconLabel>
+                    </IconWrapper>
+                </Grid>
 
+                <Grid item xs={6}>
+                    <IconWrapper>
+                      <SportsBasketballIcon fontSize="medium" />
+                      <IconLabel>Sports</IconLabel>
+                    </IconWrapper>
+                </Grid>
+
+              </IconContainer> 
+          </Grid>      
+      </Wrapper>
+    )
+  }
+
+  export default Footer;
+
+  const Wrapper = styled.div`
+      background-color: #BFDBF7;
+      text-align: left;
+      padding: 20px;
+      margin-top:100px;
+      margin-bottom:50px;
+  `;
+
+  const TextContainer = styled.div`
+      margin-bottom: 20px;
+  `;
+
+  const StyledH1 = styled.h1`
+      font-size: 20px;
+      font-weight: 700;
+    `;
+
+  const IconContainer = styled.div`
+      display: flex;
+      justify-content: space-around;
+  `;
+
+  const IconWrapper = styled.div`
+      display: flex;
+      align-items: center;
+  `;
+
+  const IconLabel = styled.p`
+      margin-top: 15px;
+      font-size: 18px;
+  `;
