c4da1f306efe31f5b17e4170bec9bde95aa25418
Rezaur Khan <rezaurkhan@Rezaurs-MacBook-Air.local>
2023-10-07 17:07:28 -0400

    Updated css

--- src/components/Homepage/Header.js
+++ src/components/Homepage/Header.js
@@ -1,10 +1,10 @@
 import React, { useState } from "react";
 import styled from "styled-components";
 import BestPrice from "../../images/best_price.jpeg";
-import LoginIcon from '@mui/icons-material/Login';
+import LoginIcon from "@mui/icons-material/Login";
 import Login from './Login';
 import SearchBar from '../Miscellaneous/SearchBar';
-import Register from './Register';  // Import the Register component
+import Register from './Register';
 
 const Header = () => {
     const [modalType, setModalType] = useState('none');  // 'none', 'login', or 'register'
@@ -18,44 +18,38 @@ const Header = () => {
                 <Title>DealDetector</Title>
             </Navbar>
             <SearchBar />
-            <LoginContainer onClick={() => setModalType('login')}>
-                <StyledIcon />
-                <StyledP>Login</StyledP>
-            </LoginContainer><RegisterContainer onClick={() => setModalType('register')}>
-                <StyledP>/Register</StyledP>
-            </RegisterContainer>
+            <ButtonContainer>
+                <Button onClick={() => setModalType('login')} login>
+                    <StyledIcon />
+                    <ButtonText>Login</ButtonText>
+                </Button>
+                <Button onClick={() => setModalType('register')}>
+                    <ButtonText>Register</ButtonText>
+                </Button>
+            </ButtonContainer>
             {modalType === 'login' && <Login closeModal={() => setModalType('none')} />}
             {modalType === 'register' && <Register closeModal={() => setModalType('none')} />}
-
         </Wrapper>
-    )
-}
-
-
+    );
+};
 
 export default Header;
 
 const Wrapper = styled.div`
     display: flex;
-    justify-content: space-between; /* Distribute content evenly */
+    justify-content: space-between;
     align-items: center;
     background-color: #BFDBF7;
     padding: 10px;
-    margin-bottom: 90px; 
-    height:70px;
+    margin-bottom: 90px;
+    height: 70px;
 `;
 
-const LoginContainer = styled.div`
-    display: flex;
-    align-items: center;
-    cursor: pointer;
-    margin:0px;
-    padding: 0;
-`;
 const Navbar = styled.div`
     display: flex;
     align-items: center;
 `;
+
 const Title = styled.h1`
     font-family: 'Pacifico', cursive;
     font-size: 24px;
@@ -65,25 +59,37 @@ const ImageContainer = styled.div`
     margin-right: 10px;
 
     img {
-        max-width: 100px; 
+        max-width: 100px;
         height: auto;
     }
 `;
-const RegisterContainer = styled.div`
+
+const ButtonContainer = styled.div`
     display: flex;
     align-items: center;
-    cursor: pointer;
-    margin: 0;
-    padding: 0;
 `;
 
-const StyledP = styled.p`
-    margin: 0; 
-    margin-left: 5px;
-    font-weight: bold;
+const Button = styled.button`
+    background-color: ${props => (props.login ? "#0366d6" : "#28a745")};
+    color: white;
+    border: none;
+    border-radius: 6px;
+    padding: 12px 16px; /* Increase padding as needed */
+    margin-right: 10px;
+    cursor: pointer;
+    display: flex;
+    align-items: center;
+    height: 36px; /* Set a fixed height for both buttons */
+
+    &:hover {
+        opacity: 0.8;
+    }
 `;
 
 const StyledIcon = styled(LoginIcon)`
-    margin: 0;
-    margin-right: -5px;  // Adjust this value if needed
+    margin-right: 8px;
+`;
+
+const ButtonText = styled.span`
+    font-weight: bold;
 `;
