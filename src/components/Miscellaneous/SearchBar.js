c0867bc24177181e83d8070c7733496a8957ee2d
Raymond <raymond.feng17@myhunter.cuny.edu>
2023-10-15 16:45:50 -0400

    added search history and delete

--- src/components/Miscellaneous/SearchBar.js
+++ src/components/Miscellaneous/SearchBar.js
@@ -11,6 +11,13 @@ const SearchBar = () => {
     const [query, setQuery] = useState('');
     const [hits, setHits] = useState([]);
     const [selectedResult, setSelectedResult] = useState(-1);
+    const [searchHistory, setSearchHistory] = useState([]);
+
+    // Load search history from local storage on component mount
+    useEffect(() => {
+        const savedHistory = JSON.parse(localStorage.getItem('searchHistory')) || [];
+        setSearchHistory(savedHistory);
+    }, []);
 
     useEffect(() => {
         if (query.trim() === '') {
@@ -34,17 +41,45 @@ const SearchBar = () => {
         setSelectedResult(-1);
     };
 
+    // Function to save searches to the search history
+    const saveToSearchHistory = (search) => {
+        const newSearchHistory = [...searchHistory];
+        if (!newSearchHistory.includes(search)) {
+            newSearchHistory.push(search);
+        }
+        setSearchHistory(newSearchHistory);
+
+        //save history
+        localStorage.setItem('searchHistory', JSON.stringify(newSearchHistory));
+    };
+
+    const deleteSearch = (index) => {
+        const updatedHistory = [...searchHistory];
+        updatedHistory.splice(index, 1);
+        setSearchHistory(updatedHistory);
+
+        //save history
+        localStorage.setItem('searchHistory', JSON.stringify(updatedHistory));
+    };
+
     const handleKeyDown = (event) => {
         if (event.key === 'ArrowDown' && selectedResult < hits.length - 1) {
             setSelectedResult(selectedResult + 1);
         } else if (event.key === 'ArrowUp' && selectedResult > 0) {
             setSelectedResult(selectedResult - 1);
-        } else if (event.key === 'Enter' && selectedResult >= 0) {
-            // Handle selection and perform an action (e.g., navigate to a page)
-            const selectedSuggestion = hits[selectedResult];
-            setQuery(selectedSuggestion.search_term);
-            setSelectedResult(-1);
-            //console.log('Selected:', selectedSuggestion);
+        } else if (event.key === 'Enter') {
+            if (selectedResult >= 0) {
+                // Handle selection and perform an action (e.g., navigate to a page)
+                const selectedSuggestion = hits[selectedResult];
+                setQuery(selectedSuggestion.search_term);
+                setSelectedResult(-1);
+            }
+            else {
+                //TODO: implement search function to API
+
+                //search result and save to history
+                saveToSearchHistory(query);
+            }
         }
     };
 
@@ -53,6 +88,15 @@ const SearchBar = () => {
         setSelectedResult(result);
         setQuery(result.search_term); // Update the query with the selected result
         setSelectedResult(-1);
+    };
+
+    //search button pressed
+    const searchInput = () => {
+        //TODO: implement search function to API
+
+        if (query.trim() !== '') {
+            saveToSearchHistory(query);
+        }
     };
 
     return (
@@ -64,9 +108,14 @@ const SearchBar = () => {
                 onChange={handleInputChange}
                 onKeyDown={handleKeyDown}
             />
-            <SearchButton>
+            <SearchButton onClick={() => searchInput()}>
                 <FiSearch />
             </SearchButton>
+            <SearchHistory>
+                {searchHistory.map((search, index) => (
+                    <li key={index} onClick={() => setQuery(search)}> {String(search)} <button onClick={() => deleteSearch(index)}>X</button> </li>
+                ))}
+            </SearchHistory>
             <SearchSuggestions>
                 {hits.map((hit, index) => (
                     <li key={hit.objectID} className={index === selectedResult ? 'selected' : ''} onClick={() => handleResultClick(hit)}>{hit.search_term}</li>
@@ -133,5 +182,36 @@ const SearchSuggestions = styled.ul`
 
     .selected {
         background-color: #f1f1f1;
+    }
+`;
+
+const SearchHistory = styled.ul`
+    display: block;
+    list-style-type: none;
+    padding: 0;
+    margin: 0;
+    position: relative;
+    width: 100%;
+    background-color: #fff;
+    border: 1px solid #ccc;
+    box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.1);
+
+    li {
+        padding: 8px;
+        cursor: pointer;
+    }
+
+    li:hover {
+        background-color: #f1f1f1;
+    }
+
+    button {
+        float: right;
+        opacity: 0.5;
+        color: black;
+        border: none;
+        padding: 2px 8px;
+        border-radius: 4px;
+        cursor: pointer;
     }
 `;
