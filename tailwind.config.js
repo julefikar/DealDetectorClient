b62862a12f4406cf139bdff1489fa1924cbe78d4
julefikar <80801138+julefikar@users.noreply.github.com>
2023-10-23 15:46:27 -0400

    Readjusted image and color config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        ashGray: '#ACBDBA',
-        azure: '#CDDDDD',
-        lilac: '#BD97C3',
-        jet: '#282929',
-        taupe: '#8E6D67'
-      }
-    }
-  },
-  variants: {},
-  plugins: [],
-}
-
+    content: ['./src/**/*.{js,jsx,ts,tsx}'],
+    theme: {
+        extend: {
+            colors: {
+                ashGray: '#ACBDBA',
+                azure: '#CDDDDD',
+                jet: '#282929',
+            },
+        },
+    },
+    variants: {},
+    plugins: [],
+};
